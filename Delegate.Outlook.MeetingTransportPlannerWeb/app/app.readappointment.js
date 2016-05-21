31dc17606266acdb870e601cc31a1bef25949c7c
Simon Pedersen <mail@sjkp.dk>
2016-05-22 00:13:33 +0200

    better layout

--- Delegate.Outlook.MeetingTransportPlannerWeb/app/app.readappointment.js
+++ Delegate.Outlook.MeetingTransportPlannerWeb/app/app.readappointment.js
@@ -14,40 +14,79 @@ var ReadAppointment = (function () {
     function ReadAppointment(ngZone, http) {
         this.ngZone = ngZone;
         this.http = http;
+        this.saveInProgress = false;
+        this.beforeMeeting = true;
         this.length = 60;
+        this.saveComplete = false;
     }
     ReadAppointment.prototype.ngOnInit = function () {
         var _this = this;
-        this.subject = "HHLLO";
+        this.recipients = Office.context.mailbox.userProfile.emailAddress;
         var appointment = Office.cast.item.toAppointmentCompose(Office.context.mailbox.item);
         appointment.start.getAsync(function (res) {
             console.log(res.value);
             _this.start = res.value;
         });
         appointment.subject.getAsync(function (res) {
-            console.log('as7cc');
-            _this.ngZone.run(function () { return _this.subject = res.value; });
+            _this.ngZone.run(function () { return _this.subject = 'Transport for ' + res.value; });
+        });
+        appointment.location.getAsync(function (res) {
+            _this.ngZone.run(function () {
+                _this.meetingLocation = res.value;
+                _this.toggleMeetingLocation(_this.beforeMeeting);
+            });
         });
     };
     ReadAppointment.prototype.search = function () {
         var _this = this;
-        this.http.get('/api/distance?origin=' + this.origin + '&destination=' + this.destination).subscribe(function (res) {
-            debugger;
+        console.log(this.origin);
+        this.http.get('/api/distance?origin=' + encodeURIComponent(this.origin) + '&destination=' + encodeURIComponent(this.destination)).subscribe(function (res) {
             var json = res.json();
+            if (json.rows[0].elements[0].status == "NOT_FOUND")
+                return;
             _this.length = Math.round(json.rows[0].elements[0].duration.value / 60);
             _this.destination = json.destination_addresses[0];
             _this.origin = json.origin_addresses[0];
         });
     };
+    ReadAppointment.prototype.onChangeBeforeMeeting = function (event) {
+        this.toggleMeetingLocation(event.currentTarget.checked);
+    };
+    ReadAppointment.prototype.toggleMeetingLocation = function (b) {
+        console.log("beforeMeeting: " + b);
+        if (b) {
+            this.origin = this.destination;
+            this.destination = this.meetingLocation;
+        }
+        else {
+            this.destination = this.origin;
+            this.origin = this.meetingLocation;
+        }
+    };
     ReadAppointment.prototype.create = function () {
+        var _this = this;
+        this.saveInProgress = true;
         console.log('create');
         var newStart = new Date(this.start.getTime());
         newStart.setMinutes(this.start.getMinutes() - this.length);
-        Office.context.mailbox.makeEwsRequestAsync(this.createAppointment(newStart, this.start, 'Transport for ' + this.subject), function (res) {
+        var body = this.createAppointment(newStart, this.start, this.subject);
+        console.log(body);
+        Office.context.mailbox.makeEwsRequestAsync(body, function (res) {
             console.log(res);
+            _this.ngZone.run(function () {
+                _this.saveComplete = true;
+                _this.saveInProgress = false;
+            });
         });
     };
+    ReadAppointment.prototype.dismissSaveComplete = function () {
+        this.saveComplete = false;
+    };
     ReadAppointment.prototype.createAppointment = function (start, end, subject) {
+        var location = (this.beforeMeeting ? this.origin : this.destination);
+        if (typeof (location) == 'undefined') {
+            location = "";
+        }
         var result = '<?xml version="1.0" encoding="utf-8"?>' +
             '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"' +
             ' xmlns:xsd="http://www.w3.org/2001/XMLSchema"' +
@@ -66,18 +105,18 @@ var ReadAppointment = (function () {
             '<Items>' +
             '<t:CalendarItem xmlns="http://schemas.microsoft.com/exchange/services/2006/types">' +
             '<Subject>' + subject + '</Subject>' +
-            '<Body BodyType="Text">Business meeting about new customers.</Body>' +
+            '<Body BodyType="Text"></Body>' +
             '<ReminderIsSet>true</ReminderIsSet>' +
             '<ReminderMinutesBeforeStart>60</ReminderMinutesBeforeStart>' +
             '<Start>' + start.toISOString() + '</Start>' +
             '<End>' + end.toISOString() + '</End>' +
             '<IsAllDayEvent>false</IsAllDayEvent>' +
             '<LegacyFreeBusyStatus>Busy</LegacyFreeBusyStatus>' +
-            '<Location>Meeting Room A</Location>' +
+            '<Location>' + location + '</Location>' +
             '<RequiredAttendees>' +
             '<Attendee>' +
             '<Mailbox>' +
-            '<EmailAddress>mail@sjkp.dk</EmailAddress>' +
+            '<EmailAddress>' + this.recipients + '</EmailAddress>' +
             '</Mailbox>' +
             '</Attendee>' +
             '</RequiredAttendees>' +
@@ -91,7 +130,7 @@ var ReadAppointment = (function () {
     ReadAppointment = __decorate([
         core_1.Component({
             selector: 'read-appointment',
-            template: "<h1>Appointment </h1> \n    {{subject}} \n        <input [(ngModel)]=\"origin\"/> \n        <input [(ngModel)]=\"destination\"/> \n    <button (click)=search()>Find driving time</button>\n\n\n    <input [(ngModel)]=\"length\"/> <button (click)=create()>Create Transport</button>"
+            templateUrl: 'app/app.readappointment.html'
         }), 
         __metadata('design:paramtypes', [core_1.NgZone, http_1.Http])
     ], ReadAppointment);
